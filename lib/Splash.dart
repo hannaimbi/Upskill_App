4e19dd054892ee8e07ce8fc736705fb670fb8950
nadiyakm <kmnadiya003@gmail.com>
2025-02-26 12:27:59 +0530

    Updated logo duration

--- lib/Splash.dart
+++ lib/Splash.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 10), () {
+    Future.delayed(const Duration(seconds: 7), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => OnboardingScreen()),
